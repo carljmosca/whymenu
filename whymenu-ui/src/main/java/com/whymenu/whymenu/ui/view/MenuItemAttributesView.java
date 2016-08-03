cafd1269c0dcbc6b99d7f4ac2c75f2b79112f2a4
Carl J. Mosca <carljmosca@gmail.com>
2016-08-03 19:50:45 -0400

    add description

--- whymenu-ui/src/main/java/com/whymenu/whymenu/ui/view/MenuItemAttributesView.java
+++ whymenu-ui/src/main/java/com/whymenu/whymenu/ui/view/MenuItemAttributesView.java
@@ -8,21 +8,29 @@ package com.whymenu.whymenu.ui.view;
 import com.vaadin.addon.touchkit.ui.NavigationView;
 import com.vaadin.addon.touchkit.ui.VerticalComponentGroup;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Notification;
 import com.whymenu.data.MenuItem;
 import com.whymenu.data.MenuItemAttribute;
+import com.whymenu.data.MenuItemAttributeOption;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author moscac
  */
-public class MenuItemAttributesView extends NavigationView {
+public class MenuItemAttributesView extends NavigationView implements Component.Listener {
 
     private final MenuItem menuItem;
+    private final List<ListSelect> listSelects;
+    private Label label;
 
     public MenuItemAttributesView(MenuItem menuItem) {
         this.menuItem = menuItem;
+        listSelects = new ArrayList<>();
         init();
     }
 
@@ -31,19 +39,42 @@ public class MenuItemAttributesView extends NavigationView {
         final VerticalComponentGroup content = new VerticalComponentGroup();
         for (MenuItemAttribute menuItemAttribute : menuItem.getAttributes()) {
             ListSelect listSelect = new ListSelect(menuItemAttribute.getDescription());
+            listSelect.addListener(this);
+            listSelect.setImmediate(true);
             listSelect.addItems(menuItemAttribute.getDetails());
             listSelect.setMultiSelect(menuItemAttribute.isMultiSelect());
             content.addComponent(listSelect);
+            listSelects.add(listSelect);
         }
+        label = new Label();
+        label.setImmediate(true);
+        content.addComponent(label);
         final Button submitButton = new Button("Add to order");
         submitButton.addClickListener((Button.ClickEvent event) -> {
             Notification.show("Thanks!");
         });
+        content.addComponent(submitButton);
         setContent(content);
     }
 
     public MenuItem getMenuItem() {
         return menuItem;
+    }
+
+    @Override
+    public void componentEvent(Event event) {
+        updateOrderedItem();
+    }
+
+    private void updateOrderedItem() {
+        StringBuilder orderItemDescription = new StringBuilder();
+        orderItemDescription.append(menuItem.getName()).append(menuItem.getDescription()).append(":");
+        listSelects.stream().filter((listSelect) -> (listSelect.getValue() != null)).map((listSelect) -> (MenuItemAttributeOption[]) listSelect.getValue()).forEach((options) -> {
+            for (MenuItemAttributeOption option : options) {
+                orderItemDescription.append(option.getDescription()).append(" ");
+            }
+        });
+        label.setValue(orderItemDescription.toString().trim());
     }
 
 }
